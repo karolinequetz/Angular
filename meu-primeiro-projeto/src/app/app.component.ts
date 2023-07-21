0d3e2b84f0da2d2f58dd614c0ed64c88d8f516b6
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-07-20 21:46:41 -0300

    feat: created project

---
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` <app-title></app-title>
+    <router-outlet></router-outlet>`,
+})
+export class AppComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log(5);
+    }, 5000);
+  }
+}
---
+++ meu-primeiro-projeto/src/app/title/title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.scss'],
+})
+export class TitleComponent implements OnInit, OnChanges {
+  @Input() public title: string = 'Bem vindo';
+  constructor() {}
+  ngOnInit(): void {}
+  ngOnChanges(): void {
+    console.log('Foi alterado com sucesso!!');
+  }
+}

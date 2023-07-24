616a163c62a12756c424802c9dc4624190576954
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-07-24 12:00:49 -0300

    feat: angular components

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` <app-title></app-title>
+  template: ` <app-title *ngIf="destruir"></app-title>
+    <br />
+    {{ valor }}
+    <button (click)="adicionar()">Adicionar</button>
+    <br />
+    <button (click)="destruirComponent()">Destruir componente</button>
     <router-outlet></router-outlet>`,
 })
-export class AppComponent implements OnInit {
+export class AppComponent
+  implements
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked
+{
+  public valor: number = 1;
+  public destruir: boolean = true;
+
   constructor() {}
-  ngOnInit(): void {
-    setTimeout(() => {
-      console.log(5);
-    }, 5000);
+
+  public adicionar(): number {
+    return (this.valor += 1);
+  }
+  public destruirComponent() {
+    this.destruir = false;
+  }
+
+  ngOnInit(): void {}
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngDoAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
   }
 }
--- meu-primeiro-projeto/src/app/title/title.component.ts
+++ meu-primeiro-projeto/src/app/title/title.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss'],
 })
-export class TitleComponent implements OnInit, OnChanges {
+export class TitleComponent implements OnInit, OnChanges, OnDestroy {
   @Input() public title: string = 'Bem vindo';
   constructor() {}
   ngOnInit(): void {}
   ngOnChanges(): void {
     console.log('Foi alterado com sucesso!!');
+  }
+  ngOnDestroy(): void {
+    console.log('Ele foi destruido!');
   }
 }

ae5c27e91908f618a7ecbd11721cae48c8da54f3
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-07-31 13:50:32 -0300

    feat: created the communication of the components with the service

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -24,6 +24,7 @@ import { Component, OnInit } from '@angular/core';
       <h2>{{ getDados.idade }}</h2>
     </ng-template>
     <app-output (enviarDados)="setDados($event)"></app-output>
+    <app-food-add></app-food-add>
     <app-food-list></app-food-list>
     <router-outlet></router-outlet>`,
 })
--- meu-primeiro-projeto/src/app/shared/shared.module.ts
+++ meu-primeiro-projeto/src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//Components
 import { NewComponentComponent } from './new-component/new-component.component';
 import { InputComponent } from './input/input.component';
 import { OutputComponent } from './output/output.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { FoodAddComponent } from './food-add/food-add.component';
 
 @NgModule({
   declarations: [
@@ -11,12 +14,14 @@ import { FoodListComponent } from './food-list/food-list.component';
     InputComponent,
     OutputComponent,
     FoodListComponent,
+    FoodAddComponent,
   ],
   exports: [
     NewComponentComponent,
     InputComponent,
     OutputComponent,
     FoodListComponent,
+    FoodAddComponent,
   ],
   imports: [CommonModule],
 })

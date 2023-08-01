49b89dbfcfd08259a6b22271e4d0c4675cfbc9df
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-08-01 10:36:03 -0300

    feat: observable and http-put and delete

--- meu-primeiro-projeto/src/app/shared/food-list/food-list.component.ts
+++ meu-primeiro-projeto/src/app/shared/food-list/food-list.component.ts
@@ -23,4 +23,24 @@ export class FoodListComponent implements OnInit {
       return this.foodList.push(res);
     });
   }
+
+  public foodListEdit(value: string, id: number) {
+    this.foodListService.foodListEdit(value, id).subscribe({
+      next: (res) => {
+        return console.log(res);
+      },
+      error: (error) => error,
+    });
+  }
+
+  public foodListDelete(id: number) {
+    return this.foodListService.foodListDelete(id).subscribe({
+      next: (res) => {
+        this.foodList = this.foodList.filter((item) => {
+          return id !== item.id;
+        });
+      },
+      error: (error) => error,
+    });
+  }
 }
--- meu-primeiro-projeto/src/app/shared/shared.module.ts
+++ meu-primeiro-projeto/src/app/shared/shared.module.ts
@@ -8,6 +8,7 @@ import { InputComponent } from './input/input.component';
 import { OutputComponent } from './output/output.component';
 import { FoodListComponent } from './food-list/food-list.component';
 import { FoodAddComponent } from './food-add/food-add.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,6 @@ import { FoodAddComponent } from './food-add/food-add.component';
     FoodListComponent,
     FoodAddComponent,
   ],
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, FormsModule],
 })
 export class SharedModule {}

29482f96854714e406c362d1c97fdf30b85d50a8
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-08-02 11:46:49 -0300

    feat: created project

---
+++ routes/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+//pages
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    PageErrorComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}

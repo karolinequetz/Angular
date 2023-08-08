b4a868f76a083aa4f16a26f6905bbb6c81e2207a
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-08-08 11:21:18 -0300

    fix: removed tests

--- teste-unitario/src/app/app.component.spec.ts
+++ teste-unitario/src/app/app.component.spec.ts
@@ -15,31 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'teste-unitario'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('teste-unitario');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'teste-unitario app is running!'
-    );
-  });
-
-  it(`Testando função de soma()'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.soma(1, 1)).toEqual(2);
-  });
-
-  it(`Testando função de soma() dando valor errado'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.soma(1, 2)).not.toEqual(2);
-  });
 });
--- teste-unitario/src/app/app.component.ts
+++ teste-unitario/src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'teste-unitario';
-
-  public soma(value1: number, value2: number) {
-    return value1 + value2;
-  }
-}
+export class AppComponent {}

c0894893c2e47c30e35bfbb399846f70abdd6ae8
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-08-08 16:47:58 -0300

    feat: created tests

--- teste-unitario/src/app/shared/banking/banking.component.spec.ts
+++ teste-unitario/src/app/shared/banking/banking.component.spec.ts
@@ -8,7 +8,7 @@ describe('BankingComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [BankingComponent]
+      declarations: [BankingComponent],
     });
     fixture = TestBed.createComponent(BankingComponent);
     component = fixture.componentInstance;
@@ -17,5 +17,31 @@ describe('BankingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`(U)getPoupanca:
+  should have poupanca = 10
+  `, () => {
+    expect(component.getPoupanca).toEqual(10);
+  });
+
+  it(`(U)getCarteira:
+  should have carteira = 50
+  `, () => {
+    expect(component.getCarteira).toEqual(50);
+  });
+
+  it(`(U)setSacar:
+  should transfer poupanca from carteira
+  `, () => {
+    component.setSacar('10');
+    expect(component.getPoupanca).toEqual(0);
+    expect(component.getCarteira).toEqual(60);
+  });
+
+  it(`(U) setDepositar(): should transfer carteira from poupanca`, () => {
+    component.setDepositar('50');
+    expect(component.getCarteira).toEqual(0);
+    expect(component.getPoupanca).toEqual(60);
   });
 });

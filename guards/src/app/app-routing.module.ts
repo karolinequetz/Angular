a24d342fea23eb8b6f501b76e0f0a13320e6d217
karolsouzaq@gmail.com <karolsouzaq@gmail.com>
2023-10-14 12:05:12 -0300

    feat: created guards

---
+++ guards/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//components Pages
+import { HomeComponent } from './shared/home/home.component';
+import { AccountComponent } from './shared/account/account.component';
+
+//Guards
+import { CanActiveGuard } from './shared/guards/can-active.guard';
+
+const routes: Routes = [{
+  path:'',
+  component:HomeComponent
+},
+{
+  path:'account',
+  component:AccountComponent,
+  canActivate:[CanActiveGuard]
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
